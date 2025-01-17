1b45205658c0ef4bb8847cb92be9325c115249bb
nortan25 <britto190499@gmail.com>
2025-01-17 15:04:18 -0300

    YU

--- public/js/index.js
+++ public/js/index.js
@@ -11,22 +11,20 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('chatForm').addEventListener('submit', broadcastChatMessage);
     document.getElementById('leave').addEventListener('click', leave);
 
-    navigator.mediaDevices.getUserMedia({ 
-        video: {
-            height: 480,
-            width: 640
-        }, 
-        audio: true 
-    })
+    navigator.mediaDevices.getUserMedia({ video: {
+        height: 480,
+        width: 640
+    }, audio: true })
     .then(function (stream) {
-        myStream = stream;
-        setLocalPlayerStream();
-        showForm();
+        myStream = stream
+        setLocalPlayerStream()
+        showForm()
     }).catch(function (err) {
-        console.log(err);
-        showFail();
-    });
-}, false);
+        console.log(err)
+        showFail()
+    })
+}, false)
+
 
 function initServerConnection() {
     socket = io(); // Não é necessário passar o nome da sala aqui
@@ -46,43 +44,43 @@ function initServerConnection() {
     });
     
     socket.on('call',  function (data) {
-        let user = new User(data.id);
-        user.pc = createPeer(user);
-        users.set(data.id, user);
+        let user = new User(data.id)
+        user.pc = createPeer(user)
+        users.set(data.id, user)
 
-        createOffer(user, socket);
-    });
+        createOffer(user, socket)
+    })
 
     socket.on('offer',  function (data) {
-        var user = users.get(data.id);
+        var user = users.get(data.id)
         if (user) {
-            answerPeer(user, data.offer, socket);
+            answerPeer(user, data.offer, socket)
         } else {
-            let user = new User(data.id);
-            user.pc = createPeer(user);
-            users.set(data.id, user);
-            answerPeer(user, data.offer, socket);
+            let user = new User(data.id)
+            user.pc = createPeer(user)
+            users.set(data.id, user)
+            answerPeer(user, data.offer, socket)
         }
-    });
+    })
 
     socket.on('answer',  function (data) {
-        var user = users.get(data.id);
+        var user = users.get(data.id)
         if(user) {
-            user.pc.setRemoteDescription(data.answer);
+            user.pc.setRemoteDescription(data.answer)
         }
-    });
+    })
 
     socket.on('candidate', function (data) {
-        var user = users.get(data.id);
+        var user = users.get(data.id)
         if (user) {
-            user.pc.addIceCandidate(data.candidate);
+            user.pc.addIceCandidate(data.candidate)
         } else {
-            let user = new User(data.id);
-            user.pc = createPeer(user);
-            user.pc.addIceCandidate(data.candidate);
-            users.set(data.id, user);
+            let user = new User(data.id)
+            user.pc = createPeer(user)
+            user.pc.addIceCandidate(data.candidate)
+            users.set(data.id, user)
         }
-    });
+    })
 
     socket.on('connect', function () {
         showPlayers();
