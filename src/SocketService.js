9817b20463028cc22f32dc1567bd6c6b4e202264
nortan25 <britto190499@gmail.com>
2025-01-17 14:12:13 -0300

    hj

--- src/SocketService.js
+++ src/SocketService.js
@@ -4,26 +4,24 @@ const EVENT_OFFER = 'offer'
 const EVENT_ANSWER = 'answer'
 const EVENT_CANDIDATE = 'candidate'
 const EVENT_DISCONNECT_USER = 'disconnect-user'
-
 const EVENT_DISCONNECT = 'disconnect'
 
 class SocketService {
     constructor(http) {
         this.init(http)
-        this.waitingList = []; // Lista de espera
+        this.waitingList = [] // Lista de espera
     }
 
     init(http) {
         this.io = require('socket.io')(http)
-        
 
         this.io.on(EVENT_CONNECTION, (socket) => {
-            const room = `room-${socket.id}` 
+            const room = `room-${socket.id}`
             console.log(`${socket.id} conectado`)
             this.waitingList.push(socket)
             socket.emit('waiting', { message: 'Você está na fila de espera.' })
 
-
+            // Quando houver pelo menos dois usuários na lista, cria uma sala
             if (this.waitingList.length >= 2) {
                 const user1 = this.waitingList.shift() // Pega o primeiro usuário
                 const user2 = this.waitingList.shift() // Pega o segundo usuário
@@ -32,53 +30,71 @@ class SocketService {
                 user1.join(roomName)
                 user2.join(roomName)
                 console.log(`Sala ${roomName} criada com ${user1.id} e ${user2.id}`)
-                 // Notifica ambos os usuários para trocarem ofertas
+                
+                // Notifica ambos os usuários para trocarem ofertas
                 user1.emit(EVENT_CALL, { id: user2.id })
                 user2.emit(EVENT_CALL, { id: user1.id })
 
-
-
-            
-            
-            
-             
-
-                socket.on(EVENT_OFFER, (data) => {
-                    console.log(`${socket.id} offering ${data.id}`)
-                    socket.to(data.id).emit(EVENT_OFFER, {
-                        id: socket.id,
+                // Evento de oferta
+                user1.on(EVENT_OFFER, (data) => {
+                    console.log(`${user1.id} oferecendo para ${data.id}`)
+                    user2.emit(EVENT_OFFER, {
+                        id: user1.id,
                         offer: data.offer
                     })
                 })
 
-                socket.on(EVENT_ANSWER, (data) => {
-                    console.log(`${socket.id} answering ${data.id}`)
-                    socket.to(data.id).emit(EVENT_ANSWER, {
-                        id: socket.id,
+                user2.on(EVENT_OFFER, (data) => {
+                    console.log(`${user2.id} oferecendo para ${data.id}`)
+                    user1.emit(EVENT_OFFER, {
+                        id: user2.id,
+                        offer: data.offer
+                    })
+                })
+
+                // Evento de resposta
+                user1.on(EVENT_ANSWER, (data) => {
+                    console.log(`${user1.id} respondendo para ${data.id}`)
+                    user2.emit(EVENT_ANSWER, {
+                        id: user1.id,
                         answer: data.answer
                     })
                 })
 
-                socket.on(EVENT_CANDIDATE, (data) => {
-                    console.log(`${socket.id} enviando candidato para ${data.id}`)
-                    
-                    // Verifique se o candidato foi enviado anteriormente, ou adicione lógica para esperar a resposta do outro lado
-                    if (socket.handshake.query.ready) { 
-                        socket.to(data.id).emit(EVENT_CANDIDATE, {
-                            id: socket.id,
-                            candidate: data.candidate
-                        })
-                    } else {
-                        console.log(`${socket.id} aguardando resposta de ICE antes de enviar candidato`)
-                    }
-                })
-                
-
-                socket.on(EVENT_DISCONNECT, () => {
-                    console.log(`${socket.id} disconnected`)
-                    this.io.emit(EVENT_DISCONNECT_USER, {
-                        id: socket.id
+                user2.on(EVENT_ANSWER, (data) => {
+                    console.log(`${user2.id} respondendo para ${data.id}`)
+                    user1.emit(EVENT_ANSWER, {
+                        id: user2.id,
+                        answer: data.answer
                     })
+                })
+
+                // Evento de candidato
+                user1.on(EVENT_CANDIDATE, (data) => {
+                    console.log(`${user1.id} enviando candidato para ${data.id}`)
+                    user2.emit(EVENT_CANDIDATE, {
+                        id: user1.id,
+                        candidate: data.candidate
+                    })
+                })
+
+                user2.on(EVENT_CANDIDATE, (data) => {
+                    console.log(`${user2.id} enviando candidato para ${data.id}`)
+                    user1.emit(EVENT_CANDIDATE, {
+                        id: user2.id,
+                        candidate: data.candidate
+                    })
+                })
+
+                // Evento de desconexão
+                user1.on(EVENT_DISCONNECT, () => {
+                    console.log(`${user1.id} desconectado`)
+                    this.io.emit(EVENT_DISCONNECT_USER, { id: user1.id })
+                })
+
+                user2.on(EVENT_DISCONNECT, () => {
+                    console.log(`${user2.id} desconectado`)
+                    this.io.emit(EVENT_DISCONNECT_USER, { id: user2.id })
                 })
             }
         })
