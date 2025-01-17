1301d7f27621f66c0402cc79340f1f7faf27c4d8
nortan25 <britto190499@gmail.com>
2025-01-16 23:22:42 -0300

    tr

--- public/js/page.js
+++ public/js/page.js
@@ -1,3 +1,128 @@
+var myStream
+var socket
+const users = new Map()
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Remover a necessidade de formulário para nome de sala
+    document.getElementById('chatForm').addEventListener('submit', broadcastChatMessage)
+    document.getElementById('leave').addEventListener('click', leave)
+
+    // Solicitar permissão para acessar câmera e microfone
+    navigator.mediaDevices.getUserMedia({ video: {
+        height: 480,
+        width: 640
+    }, audio: true })
+    .then(function (stream) {
+        myStream = stream
+        setLocalPlayerStream()
+        showWaitingMessage()  // Exibir a mensagem de espera
+    }).catch(function (err) {
+        console.log(err)
+        showFail()
+    })
+}, false)
+
+function initServerConnection() {
+    // Remover a necessidade de passar a sala como parâmetro
+    var socket = io()
+
+    socket.on('disconnect-user', function (data) {
+        var user = users.get(data.id)
+        if(user) {
+            users.delete(data.id)
+            user.selfDestroy()
+        }
+    })
+    
+    socket.on('call', function (data) {
+        let user = new User(data.id)
+        user.pc = createPeer(user)
+        users.set(data.id, user)
+
+        createOffer(user, socket)
+    })
+
+    socket.on('offer', function (data) {
+        var user = users.get(data.id)
+        if (user) {
+            answerPeer(user, data.offer, socket)
+        } else {
+            let user = new User(data.id)
+            user.pc = createPeer(user)
+            users.set(data.id, user)
+            answerPeer(user, data.offer, socket)
+        }
+    })
+
+    socket.on('answer', function (data) {
+        var user = users.get(data.id)
+        if(user) {
+            user.pc.setRemoteDescription(data.answer)
+        }
+    })
+
+    socket.on('candidate', function (data) {
+        var user = users.get(data.id)
+        if (user) {
+            user.pc.addIceCandidate(data.candidate)
+        } else {
+            let user = new User(data.id)
+            user.pc = createPeer(user)
+            user.pc.addIceCandidate(data.candidate)
+            users.set(data.id, user)
+        }
+    })
+    
+    socket.on('connect', function () {
+        showPlayers()
+    })
+
+    socket.on('connect_error', function(error) {
+        console.log('Connection ERROR!')
+        console.log(error)
+        leave()
+    })
+    
+    return socket
+}
+
+function showWaitingMessage() {
+    showPanel('loading')
+    hidePanel('fail')
+    hidePanel('connect')
+    hidePanel('players')
+
+    // Exibir mensagem de espera
+    var waitingMessage = document.createElement('p')
+    waitingMessage.innerHTML = 'Aguardando um parceiro para emparelhar...'
+    document.getElementById('loading').appendChild(waitingMessage)
+}
+
+function broadcastChatMessage(e) {
+    e.preventDefault()
+
+    var message = document.getElementById('inputChatMessage').value
+
+    addMessage(message)
+
+    for(var user of users.values()) {
+        user.sendMessage(message)
+    }
+
+    document.getElementById('inputChatMessage').value = ''
+}
+
+function leave() {
+    socket.close()
+    for(var user of users.values()) {
+        user.selfDestroy()
+    }
+    users.clear()
+    removeAllMessages()
+    showForm()
+}
+
 function showLoading() {
     showPanel('loading')
     hidePanel('fail')
--- src/SocketService.js
+++ src/SocketService.js
@@ -4,7 +4,6 @@ const EVENT_OFFER = 'offer'
 const EVENT_ANSWER = 'answer'
 const EVENT_CANDIDATE = 'candidate'
 const EVENT_DISCONNECT_USER = 'disconnect-user'
-
 const EVENT_DISCONNECT = 'disconnect'
 
 class SocketService {
@@ -14,47 +13,113 @@ class SocketService {
 
     init(http) {
         this.io = require('socket.io')(http)
+        this.waitingList = []  // Lista de espera para emparelhamento
 
         this.io.on(EVENT_CONNECTION, (socket) => {
-            const room = socket.handshake.query.room
-            if (!room) {
-                socket.disconnect()
-            } else {
-                console.log(`new user enter in room ${room}`)
-                socket.join(room)
-                console.log('requesting offers')
-                socket.to(room).emit(EVENT_CALL, { id: socket.id })
+            console.log(`${socket.id} connected`)
 
-                socket.on(EVENT_OFFER, (data) => {
-                    console.log(`${socket.id} offering ${data.id}`)
-                    socket.to(data.id).emit(EVENT_OFFER, {
-                        id: socket.id,
+            // Coloca o cliente na lista de espera
+            this.waitingList.push(socket)
+            console.log(`Added ${socket.id} to the waiting list.`)
+
+            // Quando houver dois clientes, emparelha-os em uma sala
+            if (this.waitingList.length >= 2) {
+                const user1 = this.waitingList.shift()
+                const user2 = this.waitingList.shift()
+
+                const roomName = `room-${Math.random().toString(36).substr(2, 9)}`  // Nome aleatório da sala
+                console.log(`Creating room ${roomName} for ${user1.id} and ${user2.id}`)
+                
+                // Coloca os dois clientes na sala
+                user1.join(roomName)
+                user2.join(roomName)
+
+                // Inicia a chamada entre os dois clientes
+                user1.emit(EVENT_CALL, { id: user2.id, room: roomName })
+                user2.emit(EVENT_CALL, { id: user1.id, room: roomName })
+
+                // Oferece para ambos
+                user1.on(EVENT_OFFER, (data) => {
+                    console.log(`${user1.id} offering ${data.id}`)
+                    user2.emit(EVENT_OFFER, {
+                        id: user1.id,
                         offer: data.offer
                     })
                 })
 
-                socket.on(EVENT_ANSWER, (data) => {
-                    console.log(`${socket.id} answering ${data.id}`)
-                    socket.to(data.id).emit(EVENT_ANSWER, {
-                        id: socket.id,
+                user2.on(EVENT_OFFER, (data) => {
+                    console.log(`${user2.id} offering ${data.id}`)
+                    user1.emit(EVENT_OFFER, {
+                        id: user2.id,
+                        offer: data.offer
+                    })
+                })
+
+                user1.on(EVENT_ANSWER, (data) => {
+                    console.log(`${user1.id} answering ${data.id}`)
+                    user2.emit(EVENT_ANSWER, {
+                        id: user1.id,
                         answer: data.answer
                     })
                 })
 
-                socket.on(EVENT_CANDIDATE, (data) => {
-                    console.log(`${socket.id} sending a candidate to ${data.id}`)
-                    socket.to(data.id).emit(EVENT_CANDIDATE, {
-                        id: socket.id,
+                user2.on(EVENT_ANSWER, (data) => {
+                    console.log(`${user2.id} answering ${data.id}`)
+                    user1.emit(EVENT_ANSWER, {
+                        id: user2.id,
+                        answer: data.answer
+                    })
+                })
+
+                user1.on(EVENT_CANDIDATE, (data) => {
+                    console.log(`${user1.id} sending a candidate to ${data.id}`)
+                    user2.emit(EVENT_CANDIDATE, {
+                        id: user1.id,
                         candidate: data.candidate
                     })
                 })
 
-                socket.on(EVENT_DISCONNECT, () => {
-                    console.log(`${socket.id} disconnected`)
-                    this.io.emit(EVENT_DISCONNECT_USER, {
-                        id: socket.id
+                user2.on(EVENT_CANDIDATE, (data) => {
+                    console.log(`${user2.id} sending a candidate to ${data.id}`)
+                    user1.emit(EVENT_CANDIDATE, {
+                        id: user2.id,
+                        candidate: data.candidate
                     })
                 })
+
+                // Tratamento de desconexão de usuários
+                user1.on(EVENT_DISCONNECT, () => {
+                    console.log(`${user1.id} disconnected`)
+                    this.removeUserFromWaitingList(user1)
+                    this.io.emit(EVENT_DISCONNECT_USER, { id: user1.id })
+                    this.cleanupRoom(roomName)
+                })
+
+                user2.on(EVENT_DISCONNECT, () => {
+                    console.log(`${user2.id} disconnected`)
+                    this.removeUserFromWaitingList(user2)
+                    this.io.emit(EVENT_DISCONNECT_USER, { id: user2.id })
+                    this.cleanupRoom(roomName)
+                })
+            }
+        })
+    }
+
+    // Função para remover um usuário da lista de espera
+    removeUserFromWaitingList(user) {
+        const index = this.waitingList.findIndex(socket => socket.id === user.id)
+        if (index !== -1) {
+            this.waitingList.splice(index, 1)
+            console.log(`${user.id} removed from waiting list`)
+        }
+    }
+
+    cleanupRoom(roomName) {
+        this.io.in(roomName).clients((error, clients) => {
+            if (error) throw error;
+            if (clients.length === 0) {
+                console.log(`Room ${roomName} is empty. Deleting room.`)
+                // Aqui, você pode fazer qualquer limpeza necessária, como liberar recursos
             }
         })
     }
