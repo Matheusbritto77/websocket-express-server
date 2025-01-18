62bee5f5a17a4d7c9b8b2e05778a528202c079bc
nortan25 <britto190499@gmail.com>
2025-01-17 22:27:29 -0300

    HJ

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
-require('./SocketService')(http)
+const express = require('express');
+const cors = require('cors'); // Importe o middleware cors
+const app = express();
+const http = require('http').createServer(app);
+require('./SocketService')(http);
 
 class App {
     constructor(port) {
-        this.port = port ? port : 443
+        this.port = port ? port : 443;
     }
 
     start() {
+        // Configuração do CORS para permitir todas as origens
+        app.use(cors());
+
         app.get('/health', (req, res) => {
             res.send({
                 status: 'UP'
-            })
-        })
-        
-        app.use(express.static('public'))
-                
+            });
+        });
+
+        app.use(express.static('public'));
+
         http.listen(this.port, () => {
-            console.log(`server up at port: ${this.port}`)
-        })
+            console.log(`server up at port: ${this.port}`);
+        });
     }
 }
 
 module.exports = (port) => {
-    return new App(port)
-}
+    return new App(port);
+};
