6533c48e6d3621f3f078d429b0e496d221dc2f8e
nortan25 <britto190499@gmail.com>
2025-01-21 21:41:05 -0300

    hjk

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
-    cors: {
-        origin: "*", // ou sua URL
-        methods: ["GET", "POST"]
-    },
-    transports: ['websocket', 'polling'], // Compatibilidade com transporte
-    allowEIO3: false // Garantir compatibilidade com a versão EIO 3
-});
-
-// Importando o serviço de socket
-require('./SocketService')(http);
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+require('./SocketService')(http)
 
 class App {
     constructor(port) {
-        this.port = port || 443;
+        this.port = port ? port : 3000
     }
 
     start() {
         app.get('/health', (req, res) => {
             res.send({
                 status: 'UP'
-            });
-        });
-
-        // Servindo arquivos estáticos
-        app.use(express.static('public'));
-
-        // Inicializando o servidor
+            })
+        })
+        
+        app.use(express.static('public'))
+                
         http.listen(this.port, () => {
-            console.log(`Server up at port: ${this.port}`);
-        });
+            console.log(`server up at port: ${this.port}`)
+        })
     }
 }
 
 module.exports = (port) => {
-    return new App(port);
-};
+    return new App(port)
+}
